54a1444bf5550a56d46b84403676f23c6a210154
ThunderSmoker <pitlawararadhya@gmail.com>
2023-01-08 19:31:21 +0530

    build file

---
+++ build/precache-manifest.4f1e7224b082d8afd2374314e5444eb6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a79d33e47abe82c4cb7",
+    "url": "/static/js/main.0a79d33e.chunk.js"
+  },
+  {
+    "revision": "4d269da49be3eb840668",
+    "url": "/static/js/1.4d269da4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b466081ab5b83ddf7730e359d970977",
+    "url": "/index.html"
+  }
+];
